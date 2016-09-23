89a648c40e0794563122dc511f18866bfdf8b80b
psaisana <saisanasp@gmail.com>
2016-09-23 15:25:00 +1000

    Assignment 2

---
+++ ITKNews/ITKNews/Articles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITKNews
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ArticleUrl { get; set; }
+    }
+}
+
---
+++ ITKNews/ITKNews/DataService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITKNews
+{
+    public static class DataService
+    {
+        public static IList<Article> GetArticles()
+        {
+            return new List<Article>
+            {
+                new Article
+                {
+                    Title = "Schroen masterclass seals South’s second NPL Victoria title",
+                    Author = "Staff Writers",
+                    ArticleUrl = "http://www.cornerflag.com.au/schroen-masterclass-seals-souths-second-npl-victoria-title/"
+                },
+                new Article
+                {
+                    Title = "Greens lose as Victoria’s national NPL struggles continue",
+                    Author = "Mark Gojszyk",
+                    ArticleUrl = "http://www.cornerflag.com.au/greens-lose-victorias-national-npl-struggles-continue/"
+                },
+                new Article
+                {
+                    Title = "NPL Victoria Team of the Season: Central Midfielders",
+                    Author = "Staff Writers",
+                    ArticleUrl = "http://www.cornerflag.com.au/npl-victoria-team-of-the-season-central-midfielders-2016/"
+                },
+                new Article
+                {
+                    Title = "NPL Victoria Team of the Season: Attacking Midfield",
+                    Author = "Staff Writers",
+                    ArticleUrl = "http://www.cornerflag.com.au/npl-victoria-team-season-attacking-midfield-2016/"
+                },
+                new Article
+                {
+                    Title = "Carley ends short Hume stay",
+                    Author = "Mark Gojszyk",
+                    ArticleUrl = "http://www.cornerflag.com.au/carley-ends-short-hume-stay/"
+                },
+                new Article
+                {
+                    Title = "Venomous Vranesevic fires Kingston past Saints",
+                    Author = "Damir Kulas",
+                    ArticleUrl = "http://www.cornerflag.com.au/venomous-vranesevic-fires-kingston-past-saints/"
+                }
+            };
+        }
+    }
+}
